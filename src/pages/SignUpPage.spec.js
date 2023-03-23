c783b9dfd60d56ba3476b2feab81a7f8247de7f4
johnneyop <johnneyop@gmail.com>
2023-03-23 10:46:53 -0500

    signup success and fail test case added

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,5 +1,5 @@
 import SignUpPage from './SignUpPage.vue';
-import { render, screen } from '@testing-library/vue';
+import { render, screen, waitFor } from '@testing-library/vue';
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import { setupServer} from "msw/node";
@@ -54,12 +54,21 @@ describe("Sign Up Page", () => {
         });
     });
     describe('Interaction', () => {
-        it("enables the button when the password and password repeat fields have same value", async () => {
+
+        const setup = async () => {
             render(SignUpPage);
+            const usernameInput = screen.queryByLabelText('Username');
+            const emailInput = screen.queryByLabelText('E-mail');
             const passwordInput = screen.queryByLabelText('Password');
             const passwordRepeatInput = screen.queryByLabelText('Password Repeat');
-            await userEvent.type(passwordInput, "Password");
-            await userEvent.type(passwordRepeatInput, "Password");
+            await userEvent.type(usernameInput, "user1");
+            await userEvent.type(emailInput, "user1@mail.com");
+            await userEvent.type(passwordInput, "P4ssword");
+            await userEvent.type(passwordRepeatInput, "P4ssword");
+        }
+
+        it("enables the button when the password and password repeat fields have same value", async () => {
+            await setup();
             const button = screen.queryByRole('button', { name: 'Sign Up'});
             expect(button).toBeEnabled();
         });
@@ -73,16 +82,7 @@ describe("Sign Up Page", () => {
             );
             server.listen();
 
-            
-            render(SignUpPage);
-            const usernameInput = screen.queryByLabelText('Username');
-            const emailInput = screen.queryByLabelText('E-mail');
-            const passwordInput = screen.queryByLabelText('Password');
-            const passwordRepeatInput = screen.queryByLabelText('Password Repeat');
-            await userEvent.type(usernameInput, "user1");
-            await userEvent.type(emailInput, "user1@mail.com");
-            await userEvent.type(passwordInput, "P4ssword");
-            await userEvent.type(passwordRepeatInput, "P4ssword");
+            await setup();
             const button = screen.queryByRole('button', { name: 'Sign Up'});
 
             await userEvent.click(button);
@@ -94,6 +94,110 @@ describe("Sign Up Page", () => {
                 email: "user1@mail.com",
                 password: "P4ssword",
             })
+        });
+        it("does not allow clicking to the button when there is an ongoing api call", 
+        async () => {
+            let counter = 0;
+            let requestBody;
+            const server = setupServer(
+                rest.post("/api/1.0/users", (req, res, ctx) => {
+                    counter += 1;
+                    return res(ctx.status(200));
+                })
+            );
+            server.listen();
+
+            await setup();
+            const button = screen.queryByRole('button', { name: 'Sign Up'});
+
+            await userEvent.click(button);
+
+            await userEvent.click(button);
+
+            server.close();
+            expect(counter).toBe(1);
+        });
+        it("displays spinner while the api request in progress", async () => {
+            const server = setupServer(
+                rest.post("/api/1.0/users", (req, res, ctx) => {
+                    return res(ctx.status(200));
+                })
+            );
+            server.listen();
+            await setup();
+
+            const button = screen.queryByRole('button', { name: 'Sign Up'});
+
+            await userEvent.click(button);
+            screen.queryByRole("status");
+
+            const spinner = screen.queryByRole("status");
+            expect(spinner).toBeInTheDocument();
+        });
+        it("it does not display spinner when there is no api request", async () => {
+            await setup();
+            const spinner = screen.queryByRole("status");
+            expect(spinner).not.toBeInTheDocument();
+        });
+        it("displays account activation information after successful sign up request", async () => {
+            const server = setupServer(
+                rest.post("/api/1.0/users", (req, res, ctx) => {
+                    return res(ctx.status(200));
+                })
+            );
+            server.listen();
+            await setup();
+
+            const button = screen.queryByRole('button', { name: 'Sign Up'});
+
+            await userEvent.click(button);
+            await server.close();
+
+            const text = await screen.findByText("Please check your e-mail to activate your account");
+            expect(text).toBeInTheDocument();
+        });
+        it("does not display account activation message before sign up request", async () => {
+            await setup();
+            const text = screen.queryByText(
+                "Please check your e-mail to activate your account"
+            );
+            expect(text).not.toBeVisible();
+        });
+        it("does not displays account activation information after failing sign up request", 
+        async () => {
+            const server = setupServer(
+                rest.post("/api/1.0/users", (req, res, ctx) => {
+                    return res(ctx.status(400));
+                })
+            );
+            await setup();
+
+            const button = screen.queryByRole('button', { name: 'Sign Up'});
+
+            await userEvent.click(button);
+            await server.close();
+
+            const text = screen.queryByText("Please check your e-mail to activate your account");            
+            expect(text).not.toBeVisible();
+        });
+        it("hides sign up form after successful sign up request", 
+        async () => {
+            const server = setupServer(
+                rest.post("/api/1.0/users", (req, res, ctx) => {
+                    return res(ctx.status(200));
+                })
+            );
+            await setup();
+            const button = screen.queryByRole('button', { name: 'Sign Up'});
+
+            const form = screen.queryByTestId("form-sign-up");
+
+            await userEvent.click(button);
+            await server.close();
+            await waitFor(() => {
+                expect(form).not.toBeVisible();
+            })
+
         });
     });
 });
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
-        <form class="card mt-5">
-        <div class="card-header">
-            <h1 class="text-center">Sign Up</h1>
-        </div>
-        <div class="card-body">
+        <form class="card mt-5" data-testid="form-sign-up" v-show="!signUpSuccess">
+            <div class="card-header">
+                <h1 class="text-center">Sign Up</h1>
+            </div>
+            <div class="card-body">
                 <div class="mb-3">
                     <label htmlFor="username" class="form-label">Username</label>
                     <input id="username" class="form-control" v-model="username"/>
@@ -25,10 +25,16 @@
                     <input id="password-repeat" type="password" v-model="passwordRepeat" class="form-control" />
                 </div>
                 <div class="text-center">
-                    <button class="btn btn-primary" :disabled="isDisabled" @click.prevent="submit">Sign Up</button>
+                    <button class="btn btn-primary" :disabled="isDisabled || disabled" @click.prevent="submit">
+                        <span v-if="apiProgress" class="spinner-border spinner-border-sm" role="status"></span>
+                        Sign Up
+                    </button>
                 </div>
             </div>
         </form>
+        <div class="alert alert-success" v-show="signUpSuccess">
+            Please check your e-mail to activate your account
+        </div>
     </div>
 </template>
 <script>
@@ -37,19 +43,29 @@ export default {
     name: 'SignUpPage',
     data() {
         return {
+            disabled: false,
             username: "",
             email: "",
             password: "",
             passwordRepeat: "",
+            apiProgress: false,
+            signUpSuccess: false,
         };
     },
     methods: {
         submit() {
-            axios.post("/api/1.0/users", {
+            this.disabled = true;
+            this.apiProgress = true;
+            axios
+            .post("/api/1.0/users", {
                 username: this.username,
                 email: this.email,
                 password: this.password,
             })
+            .then(() => {
+                this.signUpSuccess = true;
+            })
+            .catch(() => {});
         },
     },
     computed: {
